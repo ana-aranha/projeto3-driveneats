9f3c3eb55caeef8fad8547eeafdcb51f9966ef0d
Aranha <anab.aranha@gmail.com>
2022-06-10 02:25:47 -0300

    Testes JS

---
+++ script.js
@@ -0,0 +1,13 @@
+let selection = document.querySelectorAll(".foods");
+
+function selected(i){
+    selection[i].classList.toggle("marked")
+}
+
+// function selected(){
+//     for (i=0; i<selection.length; i++){
+//         if (selection[i]===Elemento selecionado){
+//             selection[i].classList.toggle("marked")}
+//     };
+// }
+
