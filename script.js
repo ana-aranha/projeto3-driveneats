bb19a0fb720d3de87b409ff29b078bb2e41bf73f
Aranha <anab.aranha@gmail.com>
2022-06-11 15:49:06 -0300

    Second Day Javascrip 1stPart

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
 let firstOptions = document.querySelector("div:nth-child(3)");
 let secondOptions = document.querySelector("div:nth-child(5)") 
 let thirdOptions = document.querySelector("div:nth-child(7)")
-let message = document.querySelector(".bottom > div")
+
+let firstOptionsMarked;
+let secondOptionsMarked;
+let thirdOptionsMarked;
+let foodSelected;
+let drinkSelected;
+let dessertSelected;
 
 function selectFood(elemento){
-    let firstOptionsMarked = firstOptions.querySelector(".marked")
-    if (firstOptionsMarked !== null){
+    firstOptionsMarked = firstOptions.querySelector(".marked")
+    foodSelected = elemento.innerHTML;
+    if (firstOptionsMarked !== null && firstOptionsMarked !== elemento){
         firstOptionsMarked.classList.remove("marked");
-    }
-    elemento.classList.toggle("marked");
+        elemento.classList.add("marked");
+    } else if (firstOptionsMarked !== null && firstOptionsMarked === elemento){
+        elemento.classList.remove("marked")
+    } else {
+        elemento.classList.add("marked");
+    };
     OrderedCompleted();
 }
 
 function selectDrink(elemento){
-    let secondOptionsMarked = secondOptions.querySelector(".marked")
-    if (secondOptionsMarked !== null){
+    secondOptionsMarked = secondOptions.querySelector(".marked")
+    drinkSelected = elemento.innerHTML
+    if (secondOptionsMarked !== null && secondOptionsMarked !== elemento){
         secondOptionsMarked.classList.remove("marked");
-    }
-    elemento.classList.toggle("marked");
+        elemento.classList.add("marked");
+    } else if (secondOptionsMarked !== null && secondOptionsMarked === elemento){
+        elemento.classList.remove("marked")
+    } else {
+        elemento.classList.add("marked");
+    };
     OrderedCompleted();
 }
 
 function selectDessert(elemento){
-    let thirdOptionsMarked = thirdOptions.querySelector(".marked")
-    if (thirdOptionsMarked !== null){
+    thirdOptionsMarked = thirdOptions.querySelector(".marked")
+    dessertSelected = elemento.innerHTML
+    if (thirdOptionsMarked !== null && thirdOptionsMarked !== elemento){
         thirdOptionsMarked.classList.remove("marked");
-    }
-    elemento.classList.toggle("marked");
+        elemento.classList.add("marked");
+    } else if (thirdOptionsMarked !== null && thirdOptionsMarked === elemento){
+        elemento.classList.remove("marked")
+    } else {
+        elemento.classList.add("marked");
+    };
    OrderedCompleted();
 }
 
-let text = "Pedido Final";
+
+let texto = "Pedido Final";
 let encoded = encodeURIComponent(text);
 
 // ?text=
 
 function OrderedCompleted(){
-    if (selectFood && selectDrink && selectDessert){
+    let message = document.querySelector(".bottom > div");
+    if(foodSelected && drinkSelected && dessertSelected && firstOptionsMarked === null && secondOptionsMarked === null && thirdOptionsMarked === null){
         message.classList.remove("beforeOrdered");
         message.classList.add("afterOrdered");
         message.innerHTML = "<a target='_blank' href='https://wa.me/5598981060719'><p>Fechar Pedido</p></a>";
-    };
+    } else if(firstOptionsMarked !== null || secondOptionsMarked !== null || thirdOptionsMarked !== null){
+        message.classList.add("beforeOrdered");
+        message.classList.remove("afterOrdered");
+        message.innerHTML ="<p>Selecione os 3 itens para fechar o pedido</p>"
+    }
 }
 
 
